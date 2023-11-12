4d3de60e630df6c20987752f8d24d6be82d6f2b5
GugaKMG <gugakmg1@gmail.com>
2023-11-12 18:38:34 -0300

    Adição da maioria do código pronto

---
+++ Projeto-Agenda/cabecalho.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD Contato - PHP Profissional</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="./lib/bootstrap/css/bootstrap.css">
+</head>
+<body>
+
+    <div class="container">
+        <div class="jumbotron">
+            <h1>CRUD - Contato - PHP Profissional</h1>
+        </div>
---
+++ Projeto-Agenda/index.php
@@ -0,0 +1,5 @@
+<?php
+include("cabecalho.php");
+include("nav.php");
+include("conteudo.php");
+include("rodape.php");
